bc2e7c97937a2b19a95dfb793a4a4ab99a3920a5
MASUTANI Yasuhiro <masutani@isc.osakac.ac.jp>
2018-03-06 17:15:18 +0900

    起動用バッチファイルにPATHの設定を追加，設定ファイルにCOMポートの設定を追加．

--- KobukiRTC.bat
+++ KobukiRTC.bat
@@ -1,4 +1,6 @@
 @echo off
+: libkobuki.dllを使うために必要．
+set PATH=C:\Program Files\kobuki\bin;%PATH%
 pushd %~dp0
 if not defined RTM_VC_CONFIG set RTM_VC_CONFIG=Release
 start "%~n0" "build\src\%RTM_VC_CONFIG%\%~n0Comp.exe"
